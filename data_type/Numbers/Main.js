1cbde30ce061c37eb705fecdb139ae4fcd464ab3
chethan <chethanchethu99339@gmail.com>
2024-09-10 16:27:09 +0530

    practicing the num in js

---
+++ data_type/Numbers/Main.js
@@ -0,0 +1,23 @@
+// let num = 100
+// console.log(num);
+
+// let x = 10;
+// let y = "20";
+// let z = x + y;
+
+// console.log(z);
+
+
+// let x = 123;
+// x.toString();
+// console.log((123).toString());
+// (100 + 23).toString();
+
+
+// let x = 9.656;
+// x.toExponential(2);
+// x.toExponential(4);
+// console.log(x.toExponential(1));;
+
+let m = 9.4243
+console.log(m.toFixed(1));
