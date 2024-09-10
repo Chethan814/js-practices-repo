4dfc1b2ac33fc3c35b454b276c0a9bbfc01df6b9
chethan <chethanchethu99339@gmail.com>
2024-09-10 16:09:47 +0530

    practice of array in js

---
+++ data_type/data.js
@@ -0,0 +1,7 @@
+const myDate = new Date()
+
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toUTCString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
