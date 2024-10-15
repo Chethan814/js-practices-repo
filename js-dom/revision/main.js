124c27ffda54df4e2fea3c7f5689166b3a0ebf17
chethan <chethanchethu99339@gmail.com>
2024-10-15 14:23:46 +0530

    learn to traversing

---
+++ js-dom/revision/main.js
@@ -0,0 +1,34 @@
+let parent = document.querySelector('ul');
+
+// console.log(parent);
+// console.log(parent.children);
+
+// to display the children of the ul
+
+// for (let i = 0; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerText);
+    
+// }
+
+// let arr = Array.from(parent.children)
+
+
+// arr.forEach((element) => {
+//     console.log(element.innerText);
+// });
+
+// ----------------------------------
+
+
+// console.log(parent.lastElementChild);
+
+let dayOne = document.querySelector('.days')
+
+console.log(dayOne);
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+console.log(dayOne.nextSibling);
+
+console.log(parent.childNodes);
+
+
